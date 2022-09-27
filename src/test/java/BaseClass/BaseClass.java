e1d18100a615a5bd8642a36f9754496aafaf18c0
SukreetiSharma <sukreeti.sharma@infostride.com>
2022-09-27 03:18:06 -0700

    HybridFrameWork

---
+++ src/test/java/BaseClass/BaseClass.java
@@ -0,0 +1,21 @@
+package BaseClass;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+    public  static WebDriver driver;
+
+    public static void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    public static void close(){
+        driver.close();
+    }
+}
+
