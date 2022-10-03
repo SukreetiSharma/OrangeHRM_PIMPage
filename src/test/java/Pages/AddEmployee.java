36e47b8f16c34d608823355d0937a6b36064202a
SukreetiSharma <sukreeti.sharma@infostride.com>
2022-10-03 05:52:02 -0700

    TC-02

---
+++ src/test/java/Pages/AddEmployee.java
@@ -0,0 +1,22 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class AddEmployee {
+    WebDriver driver;
+    WebDriverWait wait;
+    By AddEmployee = By.xpath("//a[contains(text(),'Add Employee')]");
+
+    public AddEmployee(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+    public void AddingEmployee(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(AddEmployee));
+        driver.findElement(AddEmployee).click();
+    }
+}
---
+++ src/test/java/StepDefinition/AddEmployee.java
@@ -0,0 +1,29 @@
+package StepDefinition;
+
+import BaseClass.BaseClass;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import java.io.IOException;
+
+public class AddEmployee extends BaseClass {
+    @Given("user is able to login and navigate to Pim page")
+    public void user_is_able_to_login_and_navigate_to_pim_page() {
+      setup();
+      obj.getUserAbleToLogin().loginPage();
+    }
+    @When("Go to Employee Page to add Employee")
+    public void go_to_employee_page_to_add_employee() {
+        obj.getAddingEmployee().AddingEmployee();
+    }
+    @When("Fill all Required Details to add Employee")
+    public void fill_all_required_details_to_add_employee() throws IOException {
+        obj.getEmployeeDetails().addDetails();
+    }
+    @Then("Verify Employee is added and Logout")
+    public void verify_employee_is_added_and_logout() {
+        obj.getEmployeeDetails().Verify();
+        obj.getUserAbleToLogout().LogOutAdmin();
+        obj.getUserAbleToLogout().closeBrowser();
+    }
+}
